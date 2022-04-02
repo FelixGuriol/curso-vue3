41129946963fc0fe2567b96b943c1ec55083ba50
Felix Gutierrez <lixfe2000g@gmail.com>
2022-04-02 17:33:08 -0500

    Section 22 finished

---
+++ 13-custom-events/src/components/ButtonContador.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="btn-contador">
+    <button @click="aumentar">{{ texto }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    texto: {
+      type: String,
+      default: "Aumentar",
+    },
+  },
+  methods:{
+    aumentar(){
+      this.$emit("accion");
+    }
+  }
+};
+</script>
+
+<style></style>
