37fa1f7462974d7e0d84e86fa369376b7c14c032
Felix Gutierrez <lixfe2000g@gmail.com>
2022-04-04 16:43:49 -0500

    Section 26 finished

--- 14-gif-route/vue.config.js
+++ 14-gif-route/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa:{//agrega configuraciones al manifest.json y al index.html (dist)
+    name: "Gif Router",
+    themeColor: "#444444",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black-translucent"
+  }
 })
