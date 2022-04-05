fb94efa8e1e4e8a538372229513a4fdbf1c7ee20
Felix Gutierrez <lixfe2000g@gmail.com>
2022-04-05 18:57:26 -0500

    Section 34 - chapter 278 finished

---
+++ 17-composition/src/components/Btn.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="accionContador">{{BtnText}}</button>
+</template>
+
+<script>
+export default {
+    props:{
+        BtnText: String
+    },
+    setup({BtnText},context){
+        const accionContador = () => {
+            context.emit("accion")
+        }
+        
+
+        return {BtnText,accionContador}
+    }
+}
+</script>
---
+++ 17-composition/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <h1>{{title}}</h1>
+  <hr>
+  <h2 :style="{color: color}">Contador: {{contador}}</h2>
+  <Btn BtnText="Aumentar" @accion="aumentar"/>
+  <Btn BtnText="Disminuir" @accion="disminuir"/>
+</div>
+</template>
+
+<script>
+import Btn from './Btn.vue'
+import {useContador} from '../hooks/useContador'
+export default {
+  name: 'HelloWorld',
+  components:{
+    Btn
+  },
+  props:{
+    msg: String
+  },
+  setup({msg}){//el primero parametro es 'props'
+    const title = msg;
+    
+
+    return {
+      title, ...useContador()
+    }
+  }
+}
+</script>
