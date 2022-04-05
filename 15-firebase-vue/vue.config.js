8c8594a566514f8d18abc4e785476dc96db2ca65
Felix Gutierrez <lixfe2000g@gmail.com>
2022-04-05 16:13:58 -0500

    Section 31 finished

--- 15-firebase-vue/vue.config.js
+++ 15-firebase-vue/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa:{//agrega configuraciones al manifest.json y al index.html (dist) "https://cli.vuejs.org/core-plugins/pwa.html#configuration"
+    name: "Gestor de Proyectos",
+    themeColor: "#03a9f4",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black-translucent"
+  }
 })
